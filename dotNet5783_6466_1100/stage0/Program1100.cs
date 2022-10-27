fcd5aadb37416e7fdfdb07f4396f83e6bafe8240
miryam peretz <miperetz@g.jct.ac.il>
2022-10-27 11:28:53 +0300

    initial code and welcome functions

---
+++ dotNet5783_6466_1100/stage0/Program1100.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace targil0
+{
+   partial class program
+    {
+        static void Main(string[] args)
+        {
+            welcome1100();
+            welcome6466();  
+            Console.ReadKey();
+
+        }
+
+        static partial void welcome6466();
+        private static void welcome1100()
+        {
+            Console.Write("enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0},welcome to my first console application", name);
+        }
+    }
+}
