096d9375cb6959ae5b93a64e258020435079d4c1
miryam peretz <miperetz@g.jct.ac.il>
2022-12-20 11:28:11 +0200

    stage2 final

--- dotNet5783_6466_1100/BL/BlImplementation/BoOrder.cs
+++ dotNet5783_6466_1100/BL/BlImplementation/BoOrder.cs
@@ -17,7 +17,9 @@ internal class BoOrder : IBoOrder
             List<DO.Order?> orderListDO = (List<DO.Order?>)dal.Order.getAll();
             BO.Order orderTempBO = new BO.Order();// create BO Order list
             DO.Order orderTempDO = dal.Order.GetByID(ID);// create DO Order list
-            IEnumerable<DO.OrderItem?> itemsListDO = dal.OrderItem.GetItemsList(orderTempDO.ID);                                            // copy details
+            IEnumerable<DO.OrderItem?> itemsListDO = dal.OrderItem.GetItemsList(orderTempDO.ID);  // orderItem list of given id          
+            
+            // copy details
             orderTempBO.ID = orderTempDO.ID;
             orderTempBO.CustomerName = orderTempDO.CustomerName;
             orderTempBO.CustomerEmail = orderTempDO.CustomerEmail;
@@ -51,11 +53,11 @@ internal class BoOrder : IBoOrder
 
     public IEnumerable<BO.OrderForList?> getOrderForList()
     {
-        List<DO.Order?> orderListDO = (List<DO.Order?>)dal.Order.getAll();
+        List<DO.Order?> orderListDO = (List<DO.Order?>)dal.Order.getAll();// get order list drom DO
 
         BO.OrderForList orderForListTemp = new BO.OrderForList();
 
-        return
+        return// create the list
             (from orderDO in orderListDO
              let orderFromBL = dal.OrderItem.GetItemsList((int)(orderDO?.ID!))
              select new BO.OrderForList()
@@ -75,8 +77,8 @@ internal class BoOrder : IBoOrder
         
         try
         {
-            DO.Order orderDO = dal.Order.GetByID(ID);// הזמנה מ DO
-            List<Tuple<DateTime? , string>>? tempTrackList = new List<Tuple<DateTime? , string>>();// רשימת צמדים
+            DO.Order orderDO = dal.Order.GetByID(ID); // order gy id from DO
+            List<Tuple<DateTime? , string>>? tempTrackList = new List<Tuple<DateTime? , string>>();// create the list
 
             if (orderDO.OrderDate != null)
             {
@@ -112,8 +114,10 @@ internal class BoOrder : IBoOrder
         
         try
         {
-            DO.Order orderDO = dal.Order.GetByID(ID);
-            IEnumerable<DO.OrderItem?>? itemsListDO = dal.OrderItem.GetItemsList(orderDO.ID); // copy details
+            DO.Order orderDO = dal.Order.GetByID(ID);// get order by id from DO
+            IEnumerable<DO.OrderItem?>? itemsListDO = dal.OrderItem.GetItemsList(orderDO.ID); 
+            
+            // copy details
             var v = (from o in itemsListDO
                      let name = dal.Product.GetByID((int)(o?.ProductID)).Name
                      select new BO.OrderItem
@@ -131,6 +135,7 @@ internal class BoOrder : IBoOrder
                 orderDO.DeliveryDate = DateTime.Now;
                 dal.Order.Update(orderDO);
             }
+            // create order to return
             BO.Order orderToReturn = new BO.Order()
             {
                 ID = orderDO.ID,
@@ -143,19 +148,11 @@ internal class BoOrder : IBoOrder
                 DeliveryDate = DateTime.Now,
                 Items =v, //(List<BO.OrderItem?>)Tools.getBOList(dal.OrderItem.GetItemsList(orderDO.ID)),
                 TotalPrice = Tools.GetTotalPrice(itemsListDO!)
-                //ID = orderDO.ID,
-                //CustomerName = orderDO.CustomerName,
-                //CustomerEmail = orderDO.CustomerEmail,
-                //CustomerAddress = orderDO.CustomerAddress,
-                //OrderStatus = BO.Status.provided,
-                //OrderDate = orderDO.OrderDate,
-                //ShipDate = orderDO.ShipDate,
-                //DeliveryDate = DateTime.Now,
-                //Items = (List<BO.OrderItem?>)dal.OrderItem.GetItemsList(orderDO.ID),
-                //TotalPrice = BlApi.Tools.GetTotalPrice(itemsListDO!)
+                
             };
-            DO.Order orderToUpdate = (DO.Order)Tools.CopyPropToStruct(orderToReturn, typeof(DO.Order));
-            dal.Order.Update(orderToUpdate);
+
+            DO.Order orderToUpdate = (DO.Order)Tools.CopyPropToStruct(orderToReturn, typeof(DO.Order));// convert BO to DO
+            dal.Order.Update(orderToUpdate);// update in DAL
             return orderToReturn;
         }
         catch (DO.DoesntExistException ex)
@@ -165,13 +162,14 @@ internal class BoOrder : IBoOrder
     }
     public BO.Order UpdateShipOrder(int ID)
     {
+        //test
         if (ID<0)
             throw new BO.InvalidInputExeption("id is out of range");
         
         try
         {
-            DO.Order orderDO = dal.Order.GetByID(ID);
-            IEnumerable<DO.OrderItem?> itemsListDO = dal.OrderItem.GetItemsList(orderDO.ID);                                            // copy details
+            DO.Order orderDO = dal.Order.GetByID(ID);// get order by id from DAL
+            IEnumerable<DO.OrderItem?> itemsListDO = dal.OrderItem.GetItemsList(orderDO.ID);  // orderItem list of order (DO)                                          // copy details
             var v = (from o in itemsListDO
                      let name = dal.Product.GetByID((int)(o?.ProductID)).Name
                      select new BO.OrderItem
